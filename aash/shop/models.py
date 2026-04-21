6dd584dd386402414402eaf50cf940444b8266b6
Munazir Abbas <munazirabbas47-dotcom@users.noreply.github.com>
2026-04-21 13:39:06 +0500

    inatial commit : add checkout models

--- aash/shop/models.py
+++ aash/shop/models.py
@@ -2,29 +2,42 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=40)
-    catagory = models.CharField(max_length=50,default="")
+    catagory = models.CharField(max_length=50, default="")
     subcatagory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     dese = models.CharField(max_length=1000)
     publish_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images",default="")
-
+    image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
-    
+
 
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
-    email = models.CharField(max_length=70,default="")
+    email = models.CharField(max_length=70, default="")
     phone = models.CharField(max_length=70, default="")
     desc = models.CharField(max_length=1000)
-    
 
     def __str__(self):
         return self.name
-    
+
+
+# create models for order
+
+
+class Orders(models.Model):
+    Order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
--- aash/shop/views.py
+++ aash/shop/views.py
@@ -1,57 +1,84 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product,Contact
+from .models import Product, Contact, Orders
 from math import ceil
+
 
 # Create your views here.
 def index(request):
-    # products = Product.objects.all()
-    # print(products)
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
-    # parms = {'numberofSlides': nSlide , 'range' : range(1,nSlide) , 'product' : products}
-    # allProds = [[products, range(1 , nSlides), nSlides],[products, range(1 , nSlides) , nSlides]]
 
     allProds = []
-    catprods = Product.objects.values('catagory', 'id')
-    cats = {item['catagory']  for item in catprods}
+    catprods = Product.objects.values("catagory", "id")
+    cats = {item["catagory"] for item in catprods}
     for cat in cats:
         prod = Product.objects.filter(catagory=cat)
         n = len(prod)
-        nSlides = n//4 + ceil((n/4)-(n//4))
-        allProds.append([prod,range(1,nSlides), nSlides])
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
 
-    parms = {'allProds' : allProds}
-    return render(request, "shop/index.html",parms)
+    parms = {"allProds": allProds}
+    return render(request, "shop/index.html", parms)
 
 
 def contact(request):
-    if request.method=="POST":
-        name = request.POST.get('name','')
-        email = request.POST.get('email','')
-        phone = request.POST.get('phone','')
-        desc = request.POST.get('desc', '')
-        # print(name,email,phone,desc)
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        desc = request.POST.get("desc", "")
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
+        thanks = True
+        return render(request, "shop/contact.html", {"thanks": thanks})
     return render(request, "shop/contact.html")
 
+
 def about(request):
-    return render(request, "shop/about.html") 
+    return render(request, "shop/about.html")
+
 
 def tracker(request):
     return render(request, "shop/tracker.html")
 
+
 def search(request):
     return render(request, "shop/search.html")
 
-def productviews(request,myid):
+
+def productviews(request, myid):
     # fetch the product using the id
     product = Product.objects.filter(id=myid)
-    return render(request, "shop/productviews.html",{'product':product[0]})
+    return render(request, "shop/productviews.html", {"product": product[0]})
+
 
 def checkout(request):
+    if request.method == "POST":
+        items_json = request.POST.get("itemsJson", "")
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        address = (
+            request.POST.get("address1", "") + " " + request.POST.get("address2", "")
+        )
+        city = request.POST.get("city", "")
+        state = request.POST.get("state", "")
+        zip_code = request.POST.get("zip_code", "")
+        phone = request.POST.get("phone", "")
+        order = Orders(
+            items_json=items_json,
+            name=name,
+            email=email,
+            address=address,
+            city=city,
+            state=state,
+            zip_code=zip_code,
+            phone=phone,
+        )
+        order.save()
+        thanks = True
+        id = order.Order_id
+        return render(request, "shop/checkout.html", {"thanks": thanks, "id": id})
     return render(request, "shop/checkout.html")
+
 
 def login(request):
     return render(request, "shop/login.html")
@@ -59,6 +86,7 @@ def login(request):
 
 def cart(request):
     return render(request, "shop/cart.html")
+
 
 def signup(request):
     return render(request, "shop/signup.html")
