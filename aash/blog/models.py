8c335cea608969a9508d24a42a3690c4a08fbcb8
Munazir Abbas <munazirabbas47-dotcom@users.noreply.github.com>
2026-04-28 12:14:51 +0500

    initail commit - Showing Object in blog page

--- aash/blog/models.py
+++ aash/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=100)
     head0 = models.CharField(max_length=5000, default="")
     chead0 = models.CharField(max_length=5000, default="")
     head1 = models.CharField(max_length=500, default="")
